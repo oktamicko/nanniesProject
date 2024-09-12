3c6ccfd29a1131ed9fbd0498a9adca6ad4bc50bc
oktamicko <graedmicko@gmail.com>
2024-09-12 10:32:47 +0700

    add ci/cd

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-# class Config:
-#     SECRET_KEY = os.environ.get("SECRET_KEY")
-#     SQLALCHEMY_DATABASE_URI = (
-#         os.environ.get("DATABASE_URI") or "mysql+pymysql://okta:okta@localhost/coba"
-#     )
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URI")
-        or "mysql+pymysql://root:okta_password@mysql-container:3306/coba"
+        os.environ.get("DATABASE_URI") or "mysql+pymysql://okta:okta@localhost/coba"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+# class Config:
+#     SECRET_KEY = os.environ.get("SECRET_KEY")
+#     SQLALCHEMY_DATABASE_URI = (
+#         os.environ.get("DATABASE_URI")
+#         or "mysql+pymysql://root:okta_password@mysql-container:3306/coba"
+#     )
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
